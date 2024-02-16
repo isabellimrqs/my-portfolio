309752a2c9c3a77af23970f48fa490a3cc90c378
Isabelli Marques <isabellimarques14@gmail.com>
2024-02-15 22:48:59 -0300

    90%

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
 
 btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir-menu')
 })
 
 menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
