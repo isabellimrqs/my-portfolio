ee0cc1df519aad0e459b12564271ebbc2e094715
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2024-02-21 16:52:30 -0300

    agora sim

---
+++ reveal.js
@@ -0,0 +1,61 @@
+window.revelar = ScrollReveal({reset:true})
+revelar.reveal('.efeito-txt-topo',
+ {
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo',
+ {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.titulo',
+ {
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-especialidades1',
+ {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-especialidades2',
+ {
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-especialidades3',
+ {
+    duration: 2000,
+    distance: '90px',
+    delay: 1200
+})
+
+revelar.reveal('.efeito-img-sobre',
+ {
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-txt-sobre',
+ {
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'right'
+})
+
+
+
+
+
